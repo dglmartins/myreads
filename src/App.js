e91f01d19944e4cbbaee27fcf6a1f2676e8363b1
danilomartins <dglmartins@outlook.com>
2017-07-30 23:16:45 -0400

    Create SearchBooks component import it into App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBooks from './SearchBooks';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <SearchBooks />
       </div>
     );
   }
